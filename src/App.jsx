ade2eaa77c36c788a4537daef749e34452a58e0a
Olexandr <118224486+sshhsa@users.noreply.github.com>
2023-06-11 16:47:25 +0300

    initial pagination

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import ImageGallery from 'components/Gallery/ImageGallery';
 import Modal from 'components/Modal/Modal';
 import Loader from 'components/Loader/Loader';
 import Message from 'components/Message/MessageEmpty';
-// import ButtonLoadMore from 'components/Button';
+import ButtonLoadMore from 'components/Button/Button';
 
 import css from './components/Style.module.css';
 
@@ -17,25 +17,33 @@ class App extends Component {
     images: [],
     isLoading: false,
     isFirstRender: true,
+    page: 1,
   };
 
   componentDidMount() {
     this.fetchImages();
   }
 
-  fetchImages = async query => {
+  fetchImages = async (query, page = 1) => {
     try {
       this.setState({ isLoading: true });
 
       const myKey = '35838965-00a6ae99c457ac18fcac9dde6';
-      const url = `https://pixabay.com/api/?key=${myKey}&q=${query}&image_type=photo&orientation=horizontal&per_page=12`;
+      const url = `https://pixabay.com/api/?key=${myKey}&q=${query}&image_type=photo&orientation=horizontal&per_page=12&page=${page}`;
       const response = await axios.get(url);
 
-      this.setState({
-        images: response.data.hits,
-      });
       if (response.data.hits.length === 0) {
         throw new Error('Backend is empty by this request');
+      }
+
+      if (page === 1) {
+        this.setState({
+          images: response.data.hits,
+        });
+      } else {
+        this.setState(prevState => ({
+          images: [...prevState.images, ...response.data.hits],
+        }));
       }
     } catch (error) {
       console.log(error);
@@ -49,6 +57,14 @@ class App extends Component {
     this.fetchImages(query);
   };
 
+  loadMoreImages = () => {
+    const { page } = this.state;
+    const nextPage = page + 1;
+    const query = '';
+
+    this.fetchImages(query, nextPage);
+  };
+
   toggleModal = () => {
     this.setState(({ showModal }) => ({
       showModal: !showModal,
@@ -56,15 +72,14 @@ class App extends Component {
   };
 
   render() {
-    const { showModal, isLoading, isFirstRender } = this.state;
+    const { showModal, isLoading, isFirstRender, images } = this.state;
+    const hasMoreImages = images.length > 0 && images.length % 12 === 0;
 
     return (
       <div className={css.container}>
         {isLoading && <Loader />}
         <Searchbar onSubmit={this.onSubmit} />
-        {/* {this.state.images.length === 0 && (
-          <Message message="Backend is empty by this request" />
-        )} */}
+
         {!isFirstRender && this.state.images.length === 0 && (
           <Message message="Backend is empty by this request" />
         )}
@@ -77,7 +92,7 @@ class App extends Component {
             </button>
           </Modal>
         )}
-        {/* <ButtonLoadMore /> */}
+        {hasMoreImages && <ButtonLoadMore onClick={this.loadMoreImages} />}
       </div>
     );
   }
---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ButtonLoadMore({ onClick }) {
+  return (
+    <button type="button" onClick={onClick}>
+      Load More
+    </button>
+  );
+}
+
+export default ButtonLoadMore;
